37790ad4791457ad3bd69302ba2b47d82c727585
jarviecc <112756261+jarviecc@users.noreply.github.com>
2022-09-18 09:51:32 +0530

    Update dev.py

--- YukkiMusic/plugins/devs/dev.py
+++ YukkiMusic/plugins/devs/dev.py
@@ -25,7 +25,7 @@ from pyrogram.types import (InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
 
 from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
+
 
 
 async def aexec(code, client, message):
